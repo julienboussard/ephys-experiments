1f4de5e2d09ba4bf871871f25ac494e2a678a3db
Charlie Windolf <cwindolf95@gmail.com>
2024-01-23 10:18:33 -0800

    Debug ibl_util

--- src/ephysx/ibl_util.py
+++ src/ephysx/ibl_util.py
@@ -1,6 +1,7 @@
 from os import symlink
 from pathlib import Path
 
+import numpy as np
 import spikeinterface.full as si
 from brainbox.io.one import SpikeSortingLoader
 from dartsort import DARTsortSorting
@@ -10,9 +11,10 @@ from one.api import ONE
 sdsc_base_path = Path("/mnt/sdceph/users/ibl/data")
 
 
-def pid2sdscpath(pid):
+def pid2sdscpath(pid, one=None):
     assert sdsc_base_path.exists()
-    one = ONE()
+    if one is None:
+        one = ONE()
 
     eid, probe = one.pid2eid(pid)
     alyx_base_path = one.eid2path(eid)
@@ -22,7 +24,9 @@ def pid2sdscpath(pid):
 
     rel_path = Path(rel_path[0])
     searchdir = (
-        sdsc_base_path / alyx_base_path.relative_to(one.cache_dir) / rel_path.parent
+        sdsc_base_path
+        / alyx_base_path.relative_to(one.cache_dir)
+        / rel_path.parent
     )
     pattern = Path(rel_path.name).with_suffix(f".*.cbin")
     glob = list(searchdir.glob(str(pattern)))
@@ -41,15 +45,20 @@ def pid2sdscpath(pid):
     return cbin_path, ch_path, meta_path
 
 
-def read_popeye_cbin_ibl(pid, symlink_folder):
+def read_popeye_cbin_ibl(pid, symlink_folder, one=None):
     """Load an IBL cbin as a spikeinterface BaseRecording on Popeye
 
     Some small files will be stored in symlink_folder, and it's okay for this
     to be on a slow drive, since they're just symlinks.
     """
-    cbin_path, ch_path, meta_path = pid2sdscpath(pid)
     symlink_folder = Path(symlink_folder)
+    symlink_folder.mkdir(exist_ok=True)
 
+    # if we already did this, use previously made symlinks
+    if list(symlink_folder.glob("*.ap.cbin")):
+        return si.read_cbin_ibl(str(symlink_folder))
+
+    cbin_path, ch_path, meta_path = pid2sdscpath(pid, one=one)
     symlink(
         cbin_path,
         symlink_folder / cbin_path.with_suffix("").with_suffix(".ap.cbin").name,
@@ -67,9 +76,9 @@ def read_popeye_cbin_ibl(pid, symlink_folder):
 
 
 def read_and_destripe_popeye_cbin_ibl(
-    pid, symlink_folder, num_chunks_per_segment=100, seed=0
+    pid, symlink_folder, one=None, num_chunks_per_segment=100, seed=0
 ):
-    rec = read_popeye_cbin_ibl(pid, symlink_folder)
+    rec = read_popeye_cbin_ibl(pid, symlink_folder, one=one)
     rec = rec.astype("float32")
     rec = si.highpass_filter(rec)
     bad_channel_ids, channel_labels = si.detect_bad_channels(
@@ -79,15 +88,24 @@ def read_and_destripe_popeye_cbin_ibl(
     rec = si.interpolate_bad_channels(rec, bad_channel_ids)
     rec = si.highpass_spatial_filter(rec)
     rec = si.zscore(
-        rec, mode="mean+std", num_chunks_per_segment=num_chunks_per_segment, seed=seed
+        rec,
+        mode="mean+std",
+        num_chunks_per_segment=num_chunks_per_segment,
+        seed=seed,
     )
     return rec
 
 
-def get_ks_sorting(pid):
-    one = ONE()
+def get_ks_sorting(pid, one=None):
+    if one is None:
+        one = ONE()
     ba = AllenAtlas()
     sl = SpikeSortingLoader(pid=pid, one=one, atlas=ba)
     spikes, clusters, channels = sl.load_spike_sorting()
-    times_samples = spikes["times"]
-    return DARTsortSorting(times_samples=times_samples, labels=clusters, channels=channels)
+    times_samples = spikes["times"].astype(int)
+    labels = spikes["clusters"].astype(int)
+    return DARTsortSorting(
+        times_samples=times_samples,
+        channels=np.zeros_like(labels),
+        labels=labels,
+    )
