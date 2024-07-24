636f118a9718a6d4de4ce97dccd41a54c569b20f
julienboussard <jb4365@columbia.edu>
2024-07-24 16:23:49 -0400

    forked repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# ephys-experiments
-Random and mostly unused and old spike sorting and other ephys code that needs a backup in case I lose my laptop
--- gen_bwm_templates.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv("~/ceph/2023_12_bwm_release.csv")
-strs = [f"{{ source /mnt/home/cwindolf/.bashrc ; mamba activate sp ; timeout 15000 python /mnt/home/cwindolf/ephys-experiments/scripts/bwm_templates.py {pid} --n_jobs 10 --retry_err  ; }}" for pid in df.pid]
-print(strs[0])
-with open("bwm_templates.sbatch", "w") as f:
-    f.write("#! /bin/env bash\n")
-    f.write("\n".join(strs))
-    f.write("\n")
